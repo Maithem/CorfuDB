a5e161e712c9dd0994d565ae175ce8201e8fff6c
Maithem <Maithem@users.noreply.github.com>
2018-10-11 23:12:45 -0700

    BlindPut Test

--- test/src/test/java/org/corfudb/integration/StreamIT.java
+++ test/src/test/java/org/corfudb/integration/StreamIT.java
@@ -1,8 +1,16 @@
 package org.corfudb.integration;
 
 import org.corfudb.runtime.CorfuRuntime;
+import org.corfudb.runtime.collections.SMRMap;
+import org.corfudb.runtime.exceptions.TransactionAbortedException;
 import org.corfudb.runtime.exceptions.WriteSizeException;
+import org.corfudb.runtime.object.transactions.TransactionType;
 import org.junit.Test;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -11,40 +19,75 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
  * A set integration tests that exercise the stream API.
  */
 
-public class StreamIT extends AbstractIT {
+public class StreamIT  {
 
     @Test
     public void largeStreamWrite() throws Exception {
-        final String host = "localhost";
-        final String streamName = "s1";
-        final int port = 9000;
+        final int numWriters = 32;
+        final int numWrites = 32_000;
+        int numClients = 1;
+        String connString = "localhost:9000";
+        final String tableName = "table1";
+        final int sizeOfPayload = 140;
+        final int batchSize = 100;
 
-        // Start node one and populate it with data
-        Process server_1 = new CorfuServerRunner()
-                .setHost(host)
-                .setPort(port)
-                .setSingle(true)
-                .runServer();
+        CorfuRuntime[] runtimes = new CorfuRuntime[numClients];
+        SMRMap<Long, byte[]>[] maps = new SMRMap[numClients];
+        for (int x = 0; x < runtimes.length; x++) {
+            runtimes[x] = new CorfuRuntime(connString).connect();
+            maps[x] = runtimes[x].getObjectsView().build()
+                    .setType(SMRMap.class)
+                    .setStreamName(tableName)
+                    .open();
+        }
 
-        final int maxWriteSize = 100;
 
-        // Configure a client with a max write limit
-        CorfuRuntime.CorfuRuntimeParameters params = CorfuRuntime.CorfuRuntimeParameters
-                .builder()
-                .maxWriteSize(maxWriteSize)
-                .build();
+        ExecutorService executors = Executors.newFixedThreadPool(numWriters + 1);
 
-        CorfuRuntime rt = CorfuRuntime.fromParameters(params);
-        rt.parseConfigurationString(host + ":" + port);
-        rt.connect();
+        long startTime = System.currentTimeMillis();
 
-        final int bufSize = maxWriteSize * 2;
 
-        // Attempt to write a payload that is greater than the configured limit.
-        assertThatThrownBy(() -> rt.getStreamsView()
-                .get(CorfuRuntime.getStreamID(streamName))
-                .append(new byte[bufSize]))
-                .isInstanceOf(WriteSizeException.class);
-        shutdownCorfuServer(server_1);
+        Future[] workersFutures = new Future[numWriters];
+        for (int x = 0; x < numWriters; x++) {
+            final int index = x % numClients;
+
+            final long base  = (x * numWrites) + numWrites + 1000;
+
+
+            workersFutures[x] = executors.submit(() -> {
+                final byte[] payload = new byte[sizeOfPayload];
+
+                int tempNumWrites = numWrites;
+                for (int i = 0; tempNumWrites >= 0; i++) {
+
+                    runtimes[index].getObjectsView().TXBuild()
+                            .setType(TransactionType.WRITE_AFTER_WRITE).begin();
+                    for (int j = 0; j < batchSize; j++) {
+                        long key = i * batchSize + j + base;
+                        maps[index].blindPut(key, payload);
+                    }
+                    runtimes[index].getObjectsView().TXEnd();
+                    tempNumWrites = tempNumWrites - batchSize;
+                }
+                //System.out.println("Thread done " + Thread.currentThread().getId());
+            });
+        }
+
+
+        for (Future future : workersFutures) {
+            future.get();
+            //System.out.println("Thread done");
+        }
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("write throughput (op/ms) " + ((numWriters * numWrites * 1.0) / (endTime - startTime)));
+
+        startTime = System.currentTimeMillis();
+        int numReads = 0;
+        for (int x = 0; x < maps.length; x++) {
+            numReads += maps[x].size();
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println("total read time " + (endTime - startTime)  + " number of entries read " + numReads);
     }
 }
